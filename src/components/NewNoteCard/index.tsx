22f39a6112dc0ce35a984bf3960db2f18f6a16ea
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-02-06 16:11:36 -0300

    feat: create `shouldShowOnboarding` state to show textarea for user write your note

--- src/components/NewNoteCard/index.tsx
+++ src/components/NewNoteCard/index.tsx
@@ -1,7 +1,19 @@
 import * as Dialog from '@radix-ui/react-dialog';
 import { X } from 'lucide-react';
+import { useState, ChangeEvent } from 'react';
 
 export function NewNoteCard() {
+  const [shouldShowOnboarding, setShouldShowOnboarding] =
+    useState<boolean>(true);
+
+  function handleStartEditor(): void {
+    setShouldShowOnboarding(false);
+  }
+
+  function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>): void {
+    if (event.target.value === '') setShouldShowOnboarding(true);
+  }
+
   return (
     <Dialog.Root>
       <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-700 p-5 gap-3 overflow-hidden outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
@@ -28,17 +40,28 @@ export function NewNoteCard() {
               Adicionar nota
             </span>
 
-            <p className='text-sm leading-6 text-slate-400'>
-              Comece{' '}
-              <button className='font-medium text-lime-400 hover:underline'>
-                gravando uma nota
-              </button>{' '}
-              em áudio ou se preferir{' '}
-              <button className='font-medium text-lime-400 hover:underline'>
-                utilize apenas texto
-              </button>
-              .
-            </p>
+            {shouldShowOnboarding ? (
+              <p className='text-sm leading-6 text-slate-400'>
+                Comece{' '}
+                <button className='font-medium text-lime-400 hover:underline'>
+                  gravando uma nota
+                </button>{' '}
+                em áudio ou se preferir{' '}
+                <button
+                  onClick={handleStartEditor}
+                  className='font-medium text-lime-400 hover:underline'
+                >
+                  utilize apenas texto
+                </button>
+                .
+              </p>
+            ) : (
+              <textarea
+                autoFocus
+                className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
+                onChange={handleContentChanged}
+              />
+            )}
           </div>
 
           <button

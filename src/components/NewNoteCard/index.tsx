b2bf2a9bccfd4ab76db55d87cb88d8d1305a1edd
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2024-02-08 15:56:07 -0300

    feat: create `handleResetEditor´ for when the modal is closed, but it hasn't note save

--- src/components/NewNoteCard/index.tsx
+++ src/components/NewNoteCard/index.tsx
@@ -18,6 +18,11 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
     setShouldShowOnboarding(false);
   }
 
+  function handleResetEditor(): void {
+    setContent('');
+    setShouldShowOnboarding(true);
+  }
+
   function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>): void {
     setContent(event.target.value);
 
@@ -108,8 +113,15 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
       <Dialog.Portal>
         <Dialog.DialogOverlay className='inset-0 fixed bg-black/50' />
 
-        <Dialog.Content className='fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none'>
-          <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
+        <Dialog.Content
+          onEscapeKeyDown={handleResetEditor}
+          onPointerDownOutside={handleResetEditor}
+          className='fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none'
+        >
+          <Dialog.Close
+            onClick={handleResetEditor}
+            className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'
+          >
             <X className='size-5' />
           </Dialog.Close>
 
